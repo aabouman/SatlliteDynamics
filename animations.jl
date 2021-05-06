e10c6b15295ac78d5275fc2e6a0b1d3a26ec3c10
Alexander A Bouman <alex.bouman@gmail.com>
2021-05-06 11:44:23 -0400

    Better Animations

--- animations.jl
+++ animations.jl
@@ -2,7 +2,7 @@
 include("MPC.jl");
 
 # %%
-N = 50; δt = 0.005;
+N = 50; δt = 0.05;
 
 Q  = Matrix(Diagonal([1.,1,1,1,1,1])) * 1.
 R  = Matrix(Diagonal([1.,1,1])) * .1
@@ -18,13 +18,13 @@ ctrl = OSQPController(Q, R, Qf, δt, N, Np, Nd);
 include("MPC.jl");
 
 mₜ = 4.709;             # Mass of satellite Megagrams
-mₛ = 5.972e21;          # Mass of earth Megagrams
-G = 8.6498928e-19;      # Gravitational constant Megameters^3/Hour^2
-earthRadius = 6.37814;  # Radius of earth Megameters
-orbitRadius = earthRadius + 1
-μ = sqrt(G * mₛ / ((orbitRadius)^3))
+mₛ = 5.97e21;           # Mass of earth Megagrams
+G = 8.65e-19;           # Gravitational constant Megameters^3/Hour^2 Megagrams
+earthRadius = 6.371;    # Radius of earth Megameters
+orbitRadius = earthRadius + 1.
+μ = sqrt(G * mₛ / ((orbitRadius)^3))    # radians / hour
 
-num_steps = 3500
+num_steps = 2000
 x_init = [-.5, 0, 0, 0.1, 0., 0]
 x_hist, u_hist = simulate(ctrl, x_init; num_steps=num_steps, verbose=false);
 
@@ -33,21 +33,29 @@ using Plots
 plot([x_hist[i][1] for i in 1:length(x_hist)])
 plot!([x_hist[i][2] for i in 1:length(x_hist)])
 
-# %% Build CSV for Kirtan
+# %%    Build CSV for Kirtan
 using DataFrames
 using CSV
 
 tmp = DataFrame(hcat(x_hist...)', ["x_tc", "y_tc", "z_tc", "vx_tc", "vy_tc", "vz_tc"]);
-CSV.write("x_hist.csv", tmp)
+CSV.write("data/x_hist_position.csv", tmp);
+
+# %%    Build Rotation matricies
+using Rotations
+
+p_st = [[orbitRadius * cos(μ * δt * (i-1)),
+         orbitRadius * sin(μ * δt * (i-1))] for i in 1:num_steps]
+p_tc = Matrix(CSV.read("data/x_hist_position.csv", DataFrame))[:, 1:3];
+
+tmp = Matrix(CSV.read("data/x_hist_quaternion.csv", DataFrame));
+R_sc = [RotMatrix(UnitQuaternion(tmp[i,1:4]))[1:2,1:2] for i in 1:size(tmp)[1]];
+R_st = [RotMatrix(UnitQuaternion(RotXYZ(tmp[i,8:10]...)))[1:2,1:2] for i in 1:size(tmp)[1]];
+# R_st = [RotMatrix(UnitQuaternion(RotXYZ(0,0,δt*i*μ)))[1:2,1:2] for i in 1:size(tmp)[1]];
 
 # %%
-plot([x_hist[i][1] for i in 1:length(x_hist)],
-     [x_hist[i][2] for i in 1:length(x_hist)])
-
+RotXYZ(UnitQuaternion(tmp[end,1:4])).theta3 % (2pi)
 # %%
-plot([u_hist[i][1] for i in 1:length(u_hist)])
-plot!([u_hist[i][2] for i in 1:length(u_hist)])
-# plot!([u_hist[i][3] for i in 1:length(u_hist)])
+RotXYZ(tmp[end,8:10]...).theta3 % (2pi)
 
 # %%
 using Plots
@@ -55,19 +63,42 @@ using Images, FileIO
 img = load("graphics/planet-earth.png")
 
 scale = .95
+tCoor = [0  1; -1 0]';
+cCoor = [0  1; -1 0]';
 
-p_st = [[orbitRadius * cos(μ * (2π) * δt * (i-1)),
-         orbitRadius * sin(μ * (2π) * δt * (i-1))] for i in 1:num_steps]
-
-anim = @animate for i ∈ 1:num_steps
-    scatter([p_st[i][1]], [p_st[i][2]], xlims=(-1.2*orbitRadius,1.2*orbitRadius),
-            ylims=(-1.2*orbitRadius,1.2*orbitRadius), aspect_ratio=:equal)
+anim = @animate for i ∈ 1:10
+    scatter([p_st[i][1]], [p_st[i][2]],
+            xlims=(-1.3*orbitRadius,1.3*orbitRadius),
+            ylims=(-1.3*orbitRadius,1.3*orbitRadius),
+            aspect_ratio=:equal,
+            label="Target")
     scatter!([p_st[i][1] + x_hist[i][1]], [p_st[i][2] + x_hist[i][2]])
+
+    quiver!([p_st[i][1]], [p_st[i][2]],
+            quiver=([(R_st[i] * tCoor)[1,1]],
+                    [(R_st[i] * tCoor)[2,1]]),
+            aspect_ratio=:equal,
+            label="Chaser"
+            color=1)
+    quiver!([p_st[i][1]], [p_st[i][2]],
+            quiver=([(R_st[i] * tCoor)[1,2]],
+                    [(R_st[i] * tCoor)[2,2]]),
+            color=2)
+
+    quiver!([p_st[i][1] + x_hist[i][1]], [p_st[i][2] + x_hist[i][2]],
+            quiver=([(R_sc[i] * cCoor)[1,1]],
+                    [(R_sc[i] * cCoor)[2,1]]),
+            color=1)
+    quiver!([p_st[i][1] + x_hist[i][1]], [p_st[i][2] + x_hist[i][2]],
+            quiver=([(R_sc[i] * cCoor)[1,2]],
+                    [(R_sc[i] * cCoor)[2,2]]),
+            color=2)
+
     plot!([-earthRadius*scale, earthRadius*scale],
           [-earthRadius*scale, earthRadius*scale],
           img[end:-1:1, :], yflip = false)
 end every 1;
 
 # %%
-fps = 30
+fps = 15
 gif(anim, "graphics/anim_fps$fps.gif", fps=fps)
